ec87c1acc616f2ddbe823884cea172ebf38b25dc
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-06-23 20:55:57 +0530

    Transaction Program JDBC

---
+++ src/com/tap/jdbc/Program.java
@@ -0,0 +1,94 @@
+package com.tap.jdbc;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Program {
+	
+	public static void main(String[] args) throws SQLException {
+
+		FileInputStream fis = null;
+		Properties p = null;
+		Connection myCon = null;
+		Statement myStmt = null;
+		ResultSet res = null;
+
+		String sql = "";
+
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			fis = new FileInputStream(
+					"C:\\Users\\moham\\eclipse-workspace\\Jdbc\\src\\com\\tap\\utility\\mySqlInfo.properties");
+			p = new Properties();
+			p.load(fis);
+
+			String url = p.getProperty("url");
+			String username = p.getProperty("username");
+			String password = p.getProperty("password");
+
+			myCon = DriverManager.getConnection(url, username, password);
+
+			int i = myStmt.executeUpdate(sql);
+
+			Employee.display(myStmt, res);
+
+		}
+
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(res, myStmt, myCon, p, fis);
+		}
+
+	}
+
+	private static void close(ResultSet res, Statement myStmt, Connection myCon, Properties p, FileInputStream fis) {
+		try {
+			if (res != null) {
+				res.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (res != null) {
+				myStmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (res != null) {
+				myCon.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}	
+
+}
