6f914cdee7a1558cdd18b97db7eec1af94a1f515
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-06-25 11:26:21 +0530

    JDCC atomic properties

--- src/com/tap/jdbc/Program.java
+++ src/com/tap/jdbc/Program.java
@@ -5,22 +5,16 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Properties;
+import java.util.Scanner;
 
 public class Program {
-	
+	static	Scanner scan = new Scanner(System.in);
+
 	public static void main(String[] args) throws SQLException {
 
 		FileInputStream fis = null;
@@ -35,8 +29,7 @@ public class Program {
 
 			Class.forName("com.mysql.cj.jdbc.Driver");
 
-			fis = new FileInputStream(
-					"C:\\Users\\moham\\eclipse-workspace\\Jdbc\\src\\com\\tap\\utility\\mySqlInfo.properties");
+			fis = new FileInputStream("C:\\Users\\moham\\eclipse-workspace\\Jdbc\\src\\com\\tap\\utility\\mySqlInfo.properties");
 			p = new Properties();
 			p.load(fis);
 
@@ -46,7 +39,11 @@ public class Program {
 
 			myCon = DriverManager.getConnection(url, username, password);
 
-			int i = myStmt.executeUpdate(sql);
+			myStmt = transaction(myCon);
+
+			//myStmt.executeUpdate(sql);
+
+			
 
 			Employee.display(myStmt, res);
 
@@ -63,6 +60,70 @@ public class Program {
 		} finally {
 			close(res, myStmt, myCon, p, fis);
 		}
+
+	}
+
+	public static Statement transaction(Connection myCon) throws SQLException{
+		Statement myStmt;
+		
+		myCon.setAutoCommit(false);
+		
+		System.out.println("Enter the senders name");
+		String sender = scan.next();
+
+		System.out.println("Enter the receiver name");
+		String receiver = scan.next();
+
+		System.out.println("Enter the amount");
+		int amount = scan.nextInt();
+
+
+		String balanceQuery="SELECT salery FROM employees WHERE name = ?";
+		PreparedStatement pstmt = myCon.prepareStatement(balanceQuery);
+		pstmt.setString(1, sender);
+		ResultSet res = pstmt.executeQuery();
+		int balance = 0;
+		if(res.next()) {
+			balance = res.getInt("salery");
+		}
+
+		int i = updateBalance(myCon, sender, -amount);
+		int j = updateBalance(myCon, receiver, amount);
+
+		boolean flag = checkTransaction(i,j,balance,amount);
+		if(flag) {
+			myCon.commit();
+			System.out.println("Transaction Successful");
+		}else {
+			myCon.rollback();
+			System.out.println("Transaction Unuccessful");
+		}
+
+		myStmt = myCon.createStatement();
+		return myStmt;
+	}
+
+	public static boolean checkTransaction(int i, int j, int balance, int amount) {
+
+		System.out.println("Do you want the transaction (yes/no)");
+		String choice = scan.next();
+		
+		return choice.equalsIgnoreCase("yes") && balance >= amount && i==1 && j==1;  
+				
+
+	}
+
+	private static int updateBalance(Connection myCon, String sender, int amount) throws SQLException {
+
+		String update = "UPDATE employees SET salery = salery + ? where name = ? ";
+		PreparedStatement pstmt = myCon.prepareStatement(update);
+
+		pstmt.setInt(1, amount);
+		pstmt.setString(2, sender);
+
+		int i = pstmt.executeUpdate();
+
+		return i;
 
 	}
 
