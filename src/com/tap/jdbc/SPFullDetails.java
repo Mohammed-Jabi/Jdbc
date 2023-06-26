074dd20140ec0883d180a35032f905fd8eb89549
Mohammed Jabir <mohammedjabi2001@gmail.com>
2023-06-26 21:47:16 +0530

    Stroed procedure in JDBC

---
+++ src/com/tap/jdbc/SPFullDetails.java
@@ -0,0 +1,100 @@
+package com.tap.jdbc;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class SPFullDetails {
+	
+	static	Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) throws SQLException {
+
+		FileInputStream fis = null;
+		Properties p = null;
+		Connection myCon = null;
+		Statement myStmt = null;
+		ResultSet res = null;
+
+		String sql = "";
+
+		try {
+
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			fis = new FileInputStream(
+					"C:\\Users\\moham\\eclipse-workspace\\Jdbc\\src\\com\\tap\\utility\\mySqlInfo.properties");
+			p = new Properties();
+			p.load(fis);
+
+			String url = p.getProperty("url");
+			String username = p.getProperty("username");
+			String password = p.getProperty("password");
+
+			myCon = DriverManager.getConnection(url, username, password);
+
+			CallableStatement call	= myCon.prepareCall("{call countemp(?,?)}");
+			
+			System.out.println("Enter the Department");
+			String department = scan.next();
+			
+			call.setString(1, department);
+			call.registerOutParameter(2,Types.INTEGER);
+			call.execute();
+			
+			int result=call.getInt(2);
+			System.out.println(result);
+
+		}
+
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(res, myStmt, myCon, p, fis);
+		}
+
+	}
+
+	private static void close(ResultSet res, Statement myStmt, Connection myCon, Properties p,
+			FileInputStream fis) {
+		try {
+			if (res != null) {
+				res.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (res != null) {
+				myStmt.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		try {
+			if (res != null) {
+				myCon.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		
+}
+
+}
